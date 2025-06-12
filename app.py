6da7293e2c7a4b0fee4a2de995259de76514d051
30305 <dessertyy18@naver.com>
2025-06-12 10:29:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-pd.read_csv("data/seoul_sales_2024.csv", encoding="cp949")
-pd.read_csv("data/seoul_subway_2024.csv", encoding="cp949")
+df_sales = pd.read_csv("data/seoul_sales_2024.csv", encoding='cp949')
+df_subway = pd.read_csv("data/seoul_subway_2024.csv", encoding='cp949')
 
 streamlit run app.py
